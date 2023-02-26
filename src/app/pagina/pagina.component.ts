4608f3bad05a1daac6deed9b297a01545d4cdf3c
isagomachado <isa_gomes_machado@hotmail.com>
2023-02-26 16:46:41 -0300

    Integração front e back

--- src/app/pagina/pagina.component.ts
+++ src/app/pagina/pagina.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { catchError } from 'rxjs/operators';
 
@@ -7,27 +7,66 @@ import { ApiService } from '../services/api.service';
 @Component({
   selector: 'app-pagina',
   templateUrl: './pagina.component.html',
-  styleUrls: ['./pagina.component.scss']
+  styleUrls: ['./pagina.component.scss'],
 })
 export class PaginaComponent implements OnInit {
-
   public apiGreeting = '';
+  public dateTime = '';
+  public text = '';
+  public showText = '';
 
-  constructor(
-    private apiService: ApiService
-  ) { }
+  constructor(private apiService: ApiService) {}
 
-  ngOnInit(): void {
-    this.apiService.getHello().pipe(
-      catchError((err) => {
-        this.apiGreeting = 'Falha na comunicação com o servidor.';
-        return [];
-      })
-    ).subscribe((response) => {
-      if (response) {
-        this.apiGreeting = response.mensagem;
-      }
-    });
+  onchangeInput(value: string) {
+    this.text = value;
   }
 
+  sendText() {
+    this.apiService
+      .postText(this.text)
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          this.dateTime = 'Falha na comunicação com o servidor.';
+          return [];
+        })
+      )
+      .subscribe((response) => {
+        this.text = '';
+        if (response) {
+          this.showText = response.mensagem;
+        }
+      });
+  }
+
+  ngOnInit(): void {
+    this.apiService
+      .getDateTime()
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          this.dateTime = 'Falha na comunicação com o servidor.';
+          return [];
+        })
+      )
+      .subscribe((response) => {
+        if (response) {
+          this.dateTime = response.mensagem;
+        }
+      });
+
+    this.apiService
+      .getHello()
+      .pipe(
+        catchError((err) => {
+          this.apiGreeting = 'Falha na comunicação com o servidor.';
+          return [];
+        })
+      )
+      .subscribe((response) => {
+        if (response) {
+          this.apiGreeting = response.mensagem;
+        }
+      });
+  }
 }
